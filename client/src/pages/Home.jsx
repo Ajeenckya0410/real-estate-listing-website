bb68b638be9380d771adfd019badd87119fe46a7
Ajeenckya0410 <132836935+Ajeenckya0410@users.noreply.github.com>
2023-11-25 18:18:52 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -71,13 +71,13 @@ export default function Home() {
       <div id="carouselExample" class="carousel slide">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="../../public/img1.jpg" class="d-block w-100" alt="..."/>
+            <img src="../../img1.jpg" class="d-block w-100" alt="..."/>
           </div>
           <div class="carousel-item">
-            <img src="../../public/img2.jpg" class="d-block w-100" alt="..."/>
+            <img src="../../img2.jpg" class="d-block w-100" alt="..."/>
           </div>
           <div class="carousel-item">
-            <img src="../../public/img3.jpg" class="d-block w-100" alt="..."/>
+            <img src="../../img3.jpg" class="d-block w-100" alt="..."/>
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
